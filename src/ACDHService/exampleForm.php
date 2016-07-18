c79093d74d61ebad77873bd3cde38b8e342255f4
nczirjak-acdh <norbert.czirjak@oeaw.ac.at>
2016-07-18 10:16:16 +0200

    Add ACDH microservice, still on developing

---
+++ src/ACDHService/exampleForm.php
@@ -0,0 +1,12 @@
+<html>
+   <body>
+   
+      <form action = "http://localhost:8282/islandora/acdh" method="post" enctype="multipart/form-data" >
+         Name: <input type = "text" name="http://aaaa.bbb/ccc" value="test" />
+         Age: <input type = "text" name = "age" />
+		 Slug: <input type = "text" name = "Slug" />*
+         <input type = "submit" />
+      </form>
+      
+   </body>
+</html>
