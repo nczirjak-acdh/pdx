fc1a98aa3d09f44a01dfdaa32efdab98e5e861ce
nczirjak-acdh <norbert.czirjak@oeaw.ac.at>
2016-07-20 08:46:50 +0200

    file uploading with Curl

--- src/ACDHService/Controller/ACDHController.php
+++ src/ACDHService/Controller/ACDHController.php
@@ -5,6 +5,8 @@ namespace Islandora\PDX\ACDHService\Controller;
 use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\FileBag;
+use Symfony\Component\HttpFoundation\File;
 use Islandora\Chullo\Uuid\IUuidGenerator;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 use ML\JsonLd\JsonLd;
@@ -43,10 +45,24 @@ class ACDHController
     
     public function metaKeys($contentType = null)
     {
+        /* two content type for example */
         /* Symphony replace the . to _ */
         $data = array();
         
-        $data = array("asd", "http://aaaa_bbb/ccc", "age", "asd");
+        if($contentType == 'XML')
+        {
+            $data = array("asd", "http://aaaa_bbb/ccc", "age", "asd");            
+        }
+        
+        if($contentType == 'XML2')
+        {
+            $data = array("asd2", "http://aaaa_bbb/ccc2", "age2", "asd");            
+        }
+        
+        if($contentType == 'XML3')
+        {
+            $data = array("asd");            
+        }
         
         return $data;
     }
@@ -65,213 +81,102 @@ class ACDHController
     }
 
     public function create(Application $app, Request $request, $id)
-    {                
+    {
         
-        //get the FORM POST VALUES
-        //ez csak egy ID
-        $tx = $request->query->get('tx', "");
+        $uplFileName = $request->files->get('fileToUpload')->getClientOriginalName();
+        $uplFileTMPPath = $request->files->get('fileToUpload')->getPathName();
+        $uplFileName = $request->files->get('fileToUpload')->getFileName();
+        //$uplFileMime = $request->files->get('fileToUpload')->getMimeType(); 
+        $uplFileMime = $request->files->get('fileToUpload')->getClientMimeType();
         
-        //Check for format - sima rdf format ellenorzes
-        $format = null;
+        $uplFileContent = file_get_contents($uplFileTMPPath);            
         
-        try 
+        if(empty($uplFileContent))
         {
-            //$format = \EasyRdf_Format::getFormat($contentType = $request->headers->get('Content-Type', 'text/turtle'));
-            $format = \EasyRdf_Format::getFormat('text/turtle');        
-        } 
-        catch (\EasyRdf_Exception $e) 
-        {            
-            $app->abort(415, $e->getMessage());
-        }
+            return false;
+        }       
+        
         
         /*
-         * 
-         * THE META DATA CGHECKING PART
-         * 
-         */
-        $objFormVal = $request->request;
+        * 
+        * THE META DATA CHECKING PART
+        * 
+        */        
+        $objFormVal = $request->request;        
         // get the formkeys
         $formKeys = $this->processFormValues($objFormVal);
         //get the schema metakeys
         $metaKeys = $this->metaKeys();
+
+        $slugValue = $this->getSlugData($objFormVal);
+/*
         //if not empty the form or meta array then compare them
         if((!empty($formKeys) && isset($formKeys)) 
-                && (!empty($metaKeys) && isset($metaKeys)) 
-          )
+               && (!empty($metaKeys) && isset($metaKeys)) 
+         )
         {              
-            $metaDiff = array_diff($metaKeys, $formKeys);                                      
-        }
+           $metaDiff = array_diff($metaKeys, $formKeys);                                      
+           /* here i need to add the sparql query for get the existing collections 
+            *  and then i can compare and select to which collections can use the uploaded file
+            */            
+  /*      }
         else
         {
-            die("Missing data");
-        }
+           die("Missing data");
+        }           
+    */   
+        /* file inserting */
+        $headers = array('Content-Type: '.$uplFileMime);    
+        $post = array('file_contents'=>$uplFileContent);        
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "http://localhost:8080/fcrepo/rest/");
+        curl_setopt($ch, CURLOPT_POST,1);        
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);    
+        $result=curl_exec ($ch);
+        curl_close ($ch);
         
-        $slugValue = $this->getSlugData($objFormVal);
-              
-              
-        //Now check if body can be parsed in that format
-        if ($format) { //EasyRdf_Format
-            
-            //Fake IRI, default LDP one for current resource "<>" is not a valid IRI!
-            $fakeUuid = $this->uuidGenerator->generateV5("derp");
-            $fakeIri = "urn:uuid:$fakeUuid";
-            $fakeParsedIri = new \EasyRdf_ParsedUri($fakeIri);
-          
-            $graph = new \EasyRdf_Graph();
-            try {                
-                $graph->parse($request->getContent(), $format->getName(), $fakeParsedIri);                                
-                $jsonld = $graph->serialise('jsonld');                
-            } catch (\EasyRdf_Exception $e) {
-                $app->abort(415, $e->getMessage());
-            }
-          
-            // Get the JSON-LD DocumentInstance.
-            $json_doc = JsonLd::getDocument($jsonld);                        
-            // Get the default graph.
-            $graph = $json_doc->getGraph();
-            // Try to get the node based on the fakeIri
-            $node = $graph->getNode($fakeIri);            
-            
-            if (is_null($node)) {
-                $nodes = $graph->getNodes();
-                if (count($nodes) == 0) {
-                    $node = $graph->createNode($fakeIri);
-                }
-            }
-            
-            if (($results = $node->getProperty('http://www.semanticdesktop.org/ontologies/2007/03/22/nfo/v1.2/uuid'))
-              !== null
-            ) {
-                if (is_array($results)) {
-                    $uuid = reset($results)->getValue();
-                } else {
-                    $uuid = $results->getValue();
-                }
-            } else {
-                $uuid = $this->uuidGenerator->generateV4();
-                $node->addPropertyValue('http://www.semanticdesktop.org/ontologies/2007/03/22/nfo/v1.2/uuid', $uuid);
-            }
-            
-            
-            $node->addPropertyValue('http://acdh.oeaw.ac.at/acdh:property', 'property11');
-
-            
-            if (($pcdm_coll = $graph->getNode('http://pcdm.org/models#ACDH')) === null) {
-                $pcdm_coll = $graph->createNode('http://pcdm.org/models#ACDH');
-            }
-                        
-            $node->addType($pcdm_coll);
-            $node->removeProperty('http://www.islandora.ca/ontologies/2016/02/28/isl/v1.0/hasURN');
-            $node->addPropertyValue(
-                'http://www.islandora.ca/ontologies/2016/02/28/isl/v1.0/hasURN',
-                'urn:uuid:'
-                . $uuid
-            );
-
-            
-            
-            //Restore LDP <> IRI on serialised graph
-            $compact = JsonLd::compact($json_doc->toJsonLd());
-            $pcdm_collection_rdf = str_replace($fakeIri, '', JsonLd::toString($compact));
-        }
-
-        $urlRoute = $request->getUriForPath('/islandora/resource/');
-
-        $subRequestPost = Request::create(
-            $urlRoute.$id,
-            'POST',
-            array(),
-            $request->cookies->all(),
-            array(),
-            $request->server->all(),
-            $pcdm_collection_rdf
-        );
-        $subRequestPost->query->set('tx', $tx);
-        $subRequestPost->headers->set('Content-Type', 'application/ld+json');
-        // Reset the Content-Length incase the end user supplied some RDF.
-        $subRequestPost->headers->set('Content-Length', strlen($pcdm_collection_rdf));
-     
-        /* addig http header infos */        
-        $serverAsd = array('HTTP_SLUG' => $slugValue);
-        $headerAsd = array('slug' => $slugValue);
-        $subRequestPost->server->add($serverAsd);
-        $subRequestPost->headers->add($headerAsd);
-        
-        $responsePost = $app->handle($subRequestPost, HttpKernelInterface::SUB_REQUEST, false);
-        
-        
-        if (201 == $responsePost->getStatusCode()) {// OK, collection created
-        
-            //Lets take the location header in the response
-            $collection_fedora_url = $responsePost->headers->get('location');
-            $indirect_container_rdf = $app['twig']->render(
-                'createIndirectContainerfromTS.json',
-                array(
-                  'resource' => $collection_fedora_url,
-                )
-            );
-            
-            $subRequestPut = Request::create(
-                $urlRoute . $id,
-                'PUT',
-                array(),
-                $request->cookies->all(),
-                array(),
-                $request->server->all(),
-                $indirect_container_rdf
-            );
-            
-            $subRequestPut->query->set('tx', $tx);
-            $subRequestPut->headers->set('Slug', 'members');
-            $subRequestPut->headers->set('Content-Type', 'application/ld+json');
-            $subRequestPut->headers->set('Content-Length', strlen($indirect_container_rdf));
-            $app['islandora.hostHeaderNormalize']($subRequestPut);
-            
-            //Here is the thing. We don't know if UUID of the collection we just created is already in the triple store.
-            //So what to do?
-            //We could just try to use our routes directly, but UUID check agains triplestore we could fail!
-            //Let's invoke the controller method directly
-            // $responsePut = $app->handle($subRequestPut, HttpKernelInterface::SUB_REQUEST, false);
-            $responsePut = $app['islandora.resourcecontroller']->put(
-                $app,
-                $subRequestPut,
-                $collection_fedora_url,
-                "members"
-            );
-            if (201 == $responsePut->getStatusCode()) {// OK, indirect container created
+        $CollectionURLMeta = $result.'/fcr:metadata';
+        $CollectionURL = $result;
                 
-                $islandora_collection_uri = $urlRoute.$uuid;
-                //Include headers from the parent one, some of the last one. Basically rewrite everything
-                $putHeaders = $responsePut->getHeaders();
-                //Guzzle psr7 response objects are inmutable. So we have to make this an array and add directly
-                $putHeaders['Link'] = array(
-                    '<'.$collection_fedora_url.'>; rel="alternate"',
-                    '<'.$urlRoute.$uuid.'/members>; rel="hub"',
-                );
-                $putHeaders['Location'] = array($islandora_collection_uri);
-                $putHeaders['Content-Length'] = strlen($islandora_collection_uri);
-                //Should i care about the etag?
-                
-                /*
-                errro_log("isandora collection");
-                error_log(print_r($islandora_collection_uri, true));
-                 */
-                error_log(print_R("headers: "));
-                error_log(print_r($putHeaders, true));
-                error_log(print_R("-----collection: "));
-                error_log(print_r($islandora_collection_uri, true));
-                
-                
-                return new Response($islandora_collection_uri, 201, $putHeaders);
-            }
-
-            return $responsePut;
-            
-        }
+        /* sparql update a form adtaok alapjan */
         
-        //Abort if PCDM collection object could not be created
-        $app->abort($responsePost->getStatusCode(), 'Failed creating PCDM Collection');
+        $data_string = "PREFIX acdh: <http://acdh.oeaw.ac.at/#>
+                                INSERT {
+                                  
+                                     <$CollectionURL> acdh:usesXSLT   <http://127.0.0.1:8080/fcrepo/rest/ContentType/TEI/XSLT> .
+                                     <$CollectionURL> acdh:providedBy <http://127.0.0.1/xsltProcessor.php> .
+                                }
+                                WHERE {}";
+        $headers = array('Content-Type: application/sparql-update');
+        $ch2 = curl_init();
+        curl_setopt($ch2, CURLOPT_URL, $CollectionURLMeta);
+        curl_setopt($ch2, CURLOPT_POSTFIELDS, $data_string);
+        curl_setopt($ch2, CURLOPT_POST,1);
+        curl_setopt($ch2, CURLOPT_CUSTOMREQUEST, 'PATCH');        
+        curl_setopt($ch2, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch2, CURLOPT_HTTPHEADER, $headers);        
+   
+        // execute the request
+        $result2=curl_exec ($ch2);        
+        curl_close ($ch2);
+  
+        var_dump("result 1: ");
+        var_dump($result);
+        var_dump("<br><br><br>");
+        var_dump("URL META: ");
+        var_dump($CollectionURLMeta);
+        var_dump("<br> MIME: ");
+        var_dump($uplFileMime);
+        var_dump("<br><br><br>");
+        var_dump("result 2: ");
+        var_dump($result2);
+        die("ittt2");
+        
+
     }
+   
     
     /**
      * Add a proxy object to the collection for the member object.
