66803a481ce8b57f7bde0fe28a195638c1826aec
root <root@islandora-deux>
2016-07-21 13:11:10 +0000

    Integrated with chullo

--- src/ACDHService/Provider/ACDHServiceProvider.php
+++ src/ACDHService/Provider/ACDHServiceProvider.php
@@ -14,121 +14,59 @@ use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\Yaml\Yaml;
 use Islandora\PDX\ACDHService\Controller\ACDHController;
 
-class ACDHServiceProvider implements ServiceProviderInterface, ControllerProviderInterface
-{
+class ACDHServiceProvider implements ServiceProviderInterface, ControllerProviderInterface {
+
     /**
-   * Part of ServiceProviderInterface
-   */
-    public function register(Application $app)
-    {
-        
+     * Part of ServiceProviderInterface
+     */
+    public function register(Application $app) {
+
         //
         // Define controller services
         //
-        //This is the base path for the application. Used to change the location
-        //of yaml config files when registerd somewhere else
+        $app['islandora.acdhcontroller'] = $app->share(function () use ($app) {
+            return new ACDHController($app);
+        });
+
+        // This is the base path for the application. Used to change the location
+        // of yaml config files when registerd somewhere else
         if (!isset($app['islandora.BasePath'])) {
-            $app['islandora.BasePath'] = __DIR__.'/..';
+            $app['islandora.BasePath'] = __DIR__ . '/..';
         }
-    
-        // If nobody registered a UuidGenerator first?
-        if (!isset($app['UuidGenerator'])) {
-            $app['UuidGenerator'] = $app->share(
-                $app->share(
-                    function () use ($app) {
-                        return new UuidGenerator();
-                    }
-                )
-            );
-        }
-        $app['islandora.acdhcontroller'] = $app->share(
-            function () use ($app) {
-                return new ACDHController($app, $app['UuidGenerator']);
-            }
-        );
-        
-        if (!isset($app['twig'])) {
-            $app['twig'] = $app->share(
-                $app->extend(
-                    'twig',
-                    function (
-                        $twig,
-                        $app
-                    ) {
-                        return $twig;
-                    }
-                )
-            );
-        } else {
-            # Add our templates to the existing twig instance.
-            $app['twig.loader']->addLoader(new \Twig_Loader_Filesystem(__DIR__ . '/../templates'));
-        }
-        if (!isset($app['api'])) {
-            $app['api'] =  $app->share(
-                function () use ($app) {
-                    return FedoraApi::create(
-                        $app['config']['islandora']['fedoraProtocol']
-                        .'://'.$app['config']['islandora']['fedoraHost']
-                        .$app['config']['islandora']['fedoraPath']
-                    );
-                }
-            );
-        }
-        if (!isset($app['triplestore'])) {
-            $app['triplestore'] = $app->share(
-                function () use ($app) {
-                    return TriplestoreClient::create(
-                        $app['config']['islandora']['tripleProtocol']
-                        .'://'.$app['config']['islandora']['tripleHost']
-                        .$app['config']['islandora']['triplePath']
-                    );
-                }
-            );
-        }
+
         /**
-    * Ultra simplistic YAML settings loader.
-    */
+         * Ultra simplistic YAML settings loader.
+         */
         if (!isset($app['config'])) {
-            $app['config'] = $app->share(
-                function () use ($app) {
-                    {
-                    if ($app['debug']) {
-                        $configFile = $app['islandora.BasePath'].'/../config/settings.dev.yml';
-                    } else {
-                        $configFile = $app['islandora.BasePath'].'/../config/settings.yml';
-                    }
-                    }
-                    $settings = Yaml::parse(file_get_contents($configFile));
-                    return $settings;
+            $app['config'] = $app->share(function () use ($app) {
+                if ($app['debug']) {
+                    $configFile = $app['islandora.BasePath'] . '/../config/settings.dev.yml';
+                } else {
+                    $configFile = $app['islandora.BasePath'] . '/../config/settings.yml';
                 }
-            );
+
+                $settings = Yaml::parse(file_get_contents($configFile));
+                return $settings;
+            });
         }
     }
 
-    public function boot(Application $app)
-    {
+    public function boot(Application $app) {
+        
     }
 
     /**
-   * Part of ControllerProviderInterface
-   */
-    public function connect(Application $app)
-    {
+     * Part of ControllerProviderInterface
+     */
+    public function connect(Application $app) {
         $ACDHControllers = $app['controllers_factory'];
         //
         // Define routing referring to controller services
         //
-
-        $ACDHControllers->post("/acdh/{id}", "islandora.acdhcontroller:create")
+        $ACDHControllers->post("/acdh/{id}", "islandora.acdhcontroller:post")
             ->value('id', "")
             ->bind('islandora.acdhCreate');
-        $ACDHControllers->post("/acdh/{id}/member/{member}", "islandora.acdhcontroller:addMember")
-            ->bind('islandora.acdhAddMember');
-        $ACDHControllers->delete(
-            "/acdh/{id}/member/{member}",
-            "islandora.acdhcontroller:removeMember"
-        )
-            ->bind('islandora.acdhRemoveMember');
         return $ACDHControllers;
     }
+
 }
--- src/index.php
+++ src/index.php
@@ -5,14 +5,12 @@ namespace Islandora\PDX;
 require_once __DIR__.'/../vendor/autoload.php';
 
 use Silex\Application;
-use Islandora\Chullo\Uuid\UuidGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Psr\Http\Message\ResponseInterface;
-use Silex\Provider\TwigServiceProvider;
 use Islandora\PDX\CollectionService\Provider\CollectionServiceProvider;
 use Islandora\Crayfish\Provider\CrayfishProvider;
+use Islandora\PDX\ACDHService\Provider\ACDHServiceProvider;
 
 date_default_timezone_set('UTC');
 
@@ -40,17 +38,13 @@ $app->register(new \Moust\Silex\Provider\CacheServiceProvider(), array(
 
 $islandoraCollectionServiceProvider = new CollectionServiceProvider;
 $islandoraCrayfishProvider = new CrayfishProvider;
+$ACDHServiceProvider = new ACDHServiceProvider;
 
-$app->register(
-    $islandoraCollectionServiceProvider,
-    array(
-        'UuidGenerator' => new UuidGenerator(),
-    )
-);
 $app->register($islandoraCrayfishProvider);
+$app->register($ACDHServiceProvider);
 
-$app->mount("/islandora", $islandoraCollectionServiceProvider);
 $app->mount("/islandora", $islandoraCrayfishProvider);
+$app->mount("/islandora", $ACDHServiceProvider);
 
 /**
  * Convert returned Guzzle responses to Symfony responses, type hinted.
@@ -68,49 +62,7 @@ $app->view(function (ResponseInterface $psr7) {
 
 //Common error Handling
 $app->error(
-    function (\EasyRdf_Exception $e, $code) use ($app) {
-        if ($app['debug']) {
-            return;
-        }
-        return new response(sprintf('RDF Library exception', $e->getMessage(), $code), $code);
-    }
-);
-$app->error(
-    function (\Symfony\Component\HttpKernel\Exception\HttpException $e, $code) use ($app) {
-        if ($app['debug']) {
-            return;
-        }
-        return new response(
-            sprintf(
-                'Islandora Collection Service exception: %s / HTTP %d response',
-                $e->getMessage(),
-                $code
-            ),
-            $code
-        );
-    }
-);
-$app->error(
-    function (\Symfony\Component\HttpKernel\Exception\NotFoundHttpException $e, $code) use ($app) {
-        if ($app['debug']) {
-            return;
-        }
-        //Not sure what the best "verbose" message is
-        return new response(
-            sprintf(
-                'Islandora Collection Service exception: %s / HTTP %d response',
-                $e->getMessage(),
-                $code
-            ),
-            $code
-        );
-    }
-);
-$app->error(
     function (\Exception $e, $code) use ($app) {
-        if ($app['debug']) {
-            return;
-        }
         return new response(
             sprintf(
                 'Islandora Collection Service uncatched exception: %s %d response',
